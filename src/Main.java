36a68cd9c8c8ad712fa008e847e0c733c46c6b13
Kevin Ernest Long <kevinelong@gmail.com>
2025-04-09 14:33:27 -0700

    more

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void greet(){
+        System.out.println("Hello World. I am in a method/function");
     }
+    public static int add(int a, int b){
+        return a + b;
+    }
+    public static String fullName(String first, String last){
+        return last + ", " + first;
+    }
+    public static String agePhrase(int age){
+        return "Your age is: " + age;
+    }
+    public static void main(String[] args) {
+       greet();
+        greet();
+        int sum = add(6,7) ; //call/invoke the function above
+        System.out.println(sum);
+        System.out.println(add(11,22));
+        System.out.println(fullName("Kevin", "Long"));
+        System.out.println(agePhrase(57));
+    }
+
 }
